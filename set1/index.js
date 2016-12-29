c1c3d91894463749c8866b08fd1e31effea66225
Miso Korkiakoski <miso.korkiakoski@reaktor.fi>
2016-12-29 10:58:38 +0200

    add expectation for challenge 5
    Signed-off-by: Miso Korkiakoski <miso.korkiakoski@reaktor.fi>

--- set1/index.js
+++ set1/index.js
@@ -52,8 +52,9 @@ function challenge5() {
     const cleartext = `Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal`
     const key = 'ICE'
     const cipher = xorCharByChar(cleartext, key)
+    const expected = '0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f'
     console.log(cipher)
-    console.log(xorCharByChar(xorCharByChar(cleartext, key), key))
+    assert.equal(cipher, expected)
 }
 
 // 4
